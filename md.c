70a766ee7d5fbe6af50969b01e4601f986048389
sdivyakpr <indhusachithanantham@gmail.com>
2016-09-17 15:12:34 +0530

    finish

---
+++ md.c
@@ -0,0 +1,22 @@
+#include<conio.h>
+long countprime(long a,long k)
+{
+long i;
+for(i=a;i>=2;i--)
+{
+return 1;
+}
+void main()
+{
+long i,range=100;
+long count=1;
+double x;
+
+for(i=3;i<range;i++)
+{
+x=sqrt(i);
+cou+=couprime(floor(x),i);
+}
+printf("\n Total number of prime numbers are %ld",count);
+getch();
+}
